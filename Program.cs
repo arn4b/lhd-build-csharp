c04109a650a15e01d3d4463095a3abf98519583e
Arnab Chatterjee <arnabc1701@gmail.com>
2021-01-11 20:33:45 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var name = "Arnab";
+            Console.WriteLine($"Iam {name}");
+        }
+    }
+}
